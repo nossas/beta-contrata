6b4bffc8a5ee1cce77c6ba26c72a6621e81f7c9e
simoneas02 <simoneas03@gmail.com>
2018-01-26 17:35:38 -0300

    change order of the import components

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 import MessagesContainer from '../containers/MessagesContainer'
 import Header from './Header'
+import MessageSearch from './MessageSearch'
 
 class App extends Component {
   render() {
     return (
       <div className='container'>
         <Header />
+        <MessageSearch />
         <MessagesContainer />
       </div>
     );
