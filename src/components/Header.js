875211b031b7b4b054da040437bf3e45b3118de3
simoneas02 <simoneas03@gmail.com>
2018-01-26 23:12:48 -0300

    fix url for git corner

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import GithubCorner from 'react-github-corner';
 
 const Header = () => (
   <header className='header'>
-    <GithubCorner size={80} bannerColor='var(--primary-color)' href='https://github.com/simoneas02/beta-contrata#readme' />
+    <GithubCorner size={80} bannerColor='var(--primary-color)' href='https://www.messenger.com/t/beta.feminista' target="_blank" />
     <div className='header__description'>
       <img className='header__img' src='./assets/img/logo.png' alt='logo Beta a Robô' />
       <h1 className='header__title'>Vem de #chat com a beta</h1>
