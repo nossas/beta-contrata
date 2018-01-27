e06301f17ff0e8104867767ba2503bd5254b3963
simoneas02 <simoneas03@gmail.com>
2018-01-26 23:11:58 -0300

    fix bug for input search

--- src/components/MessageSearch.js
+++ src/components/MessageSearch.js
@@ -1,33 +1,40 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+ 
+import { searchMessage } from "../actions";
+import Button from "./Button";
+ 
+const MessageSearch = ({ dispatch }) => {
+  let searchInput;
+ 
+  const onSubmit = event => {
+    event.preventDefault();
+    dispatch(searchMessage(searchInput.value));
+  };
 
-import { searchMessage } from '../actions';
-import Button from './Button';
-
-let MessageSearch = ({dispatch}) => {
-
-  return(
-    <div className='search'>
-      <form className='search__form'>
-        <input 
-          className='search__input' 
-          type='search' 
-          placeholder='Vê aqui o que tá rolando'
-          // aqui não consegui retornar o valor do input, rertorna um erro que parou de funcionar o botão deletar :)
-          // se fosse no react normal eu usaria pegaria com o this.refs e atualizaria o status, mas no redux não consegui retornar
-          /*ref={node => {inputSearch = node}}
-          onChange={ dispatch(searchMessage(inputSearch.value.trim().toLowerCase())) }*/                
+  return (
+    <div className="search">
+      <form className="search__form" onSubmit={onSubmit}>
+        <input
+          className="search__input"
+          type="search"
+          placeholder="Vê aqui o que tá rolando"
+          ref={node => (searchInput = node)}
         />
-
-        <Button style='btn btn--search' buttonType='submit' >
-          <img className='.btn__icon--search' src='./assets/icon/search.svg' alt='Search button' />
+ 
+        <Button style="btn btn--search" buttonType="submit">
+          <img
+            className=".btn__icon--search"
+            src="./assets/icon/search.svg"
+            alt="Search button"
+          />
         </Button>
-        
       </form>
-      <span className='search__erro search__erro--hide' ref='erro'>* O campo de pesquisa não pode ser vazio</span>
+      <span className="search__erro search__erro--hide">
+        * O campo de pesquisa não pode ser vazio
+      </span>
     </div>
   );
-}
-
-MessageSearch = connect()(MessageSearch)
-export default MessageSearch;
+};
+ 
+export default connect()(MessageSearch);
--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -1,36 +1,41 @@
-import messages from '../messages';
-
+import messages from "../messages";
+ 
 const arrangeMessages = messages => {
   const messageKey = Object.keys(messages);
-
+ 
   return messageKey.map((key, id) => ({
     key,
     id: ++id,
     message: messages[key]
   }));
-
-};                                                                                                                                        
-
-const INITIAL_STATE = {
-  list: arrangeMessages(messages),
 };
-
+ 
+const INITIAL_STATE = {
+  list: arrangeMessages(messages)
+};
+ 
 export default (state = INITIAL_STATE, action) => {
-  console.log(action)
   switch (action.type) {
-    case 'DELETE_MESSAGE':
+    case "DELETE_MESSAGE":
       return {
         ...state,
         list: state.list.filter(msg => msg.id !== action.payload)
-      }
-    
-    case 'SEARCH_MESSAGE':
-      console.log(action.payload)
+      };
+ 
+    case "SEARCH_MESSAGE":
+      console.log(action.payload);
       return {
         ...state,
-        list: state.list.filter(msg => msg.message.toLowerCase().match(action.payload))
-      }
-    
+        list: state.list.filter(msg => {
+          if (msg.message.toLowerCase) {
+            return msg.message
+              .toLowerCase()
+              .match(action.payload.toLowerCase());
+          }
+          return false;
+        })
+      };
+ 
     default:
       return state;
   }
